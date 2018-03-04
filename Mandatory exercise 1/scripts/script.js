73856602af6f610cc332ba0dac43f1662da2b510
alesja1986 <alesja.rudaja@gmail.com>
2018-03-04 01:13:08 +0100

    soon done,but some buggs still left

--- Mandatory exercise 1/scripts/script.js
+++ Mandatory exercise 1/scripts/script.js
@@ -1,7 +1,9 @@
 naviGation(); //anropar navigationens functionen
 let itemsOf;
-let valdaItemsArray = []; //array med items som valda
+$valdaItemsArray = []; //array med items som valda
 let cartItemsCount = 0; //variabel för att räknar hur många produkter är i varukorgen
+$jsonString ='{ "database" : []}';  //tum jsonstring för att när den körs första gången ska man inte få fel meddelande på grund av en tum string
+$arrayViews =[];
 
 //navigationen på sidan hanteras i denna function med jQuery
 function naviGation () {
@@ -68,18 +70,18 @@ document.getElementById('products').innerHTML=printsClothes;
 cartItems();
 //funktionen för att lägga till items i varukorgen och räkna dem
 function cartItems() {
-    let btn = document.getElementsByClassName('submit');         // reffererar till  köp knappar
+    $btn =$('.submit');         // reffererar till  köp knappar
 
-    for (var i = 0; i < btn.length; i++) {                     // går genom alla knapparna
-        btn[i].onclick = function () {                        // ger till varje knapp en event listener
+    for (var i = 0; i < $btn.length; i++) {                     // går genom alla knapparna
+        $btn[i].onclick = function () {                        // ger till varje knapp en event listener
             var valdaItemNamn = this.id;                     //valda item får this.Id som är {cloth.name}
-            if (valdaItemsArray[valdaItemNamn]) {           //om arrayn med valda produkt innerhåller redan valdaItemNamnet
-                valdaItemsArray[valdaItemNamn]++;          //ska den valdaItemNamnet plussas med 1
+            if ($valdaItemsArray[valdaItemNamn]) {           //om arrayn med valda produkt innerhåller redan valdaItemNamnet
+                $valdaItemsArray[valdaItemNamn]++;          //ska den valdaItemNamnet plussas med 1
             } else {
-                valdaItemsArray[valdaItemNamn] = 1;      //om den valdaItemNamnet inte finns ska den få 1
+                $valdaItemsArray[valdaItemNamn] = 1;      //om den valdaItemNamnet inte finns ska den få 1
             }
 
-            itemsOf = Object.getOwnPropertyNames(valdaItemsArray); //The Object.getOwnPropertyNames() method returns an array of all properties
+            itemsOf = Object.getOwnPropertyNames($valdaItemsArray); //The Object.getOwnPropertyNames() method returns an array of all properties
             itemsOf.shift(); // supper sexy monster hack för att ta bort första elementet 'lenght' som man inte ved vart den kommer ifrån:P
             cartItemsCount++;// räknare som visar hur många items man vald
             document.getElementById('visaAntalet').innerHTML = cartItemsCount;//skriver ut räknare vid varukorgen
@@ -102,7 +104,7 @@ function showItems () {
                             <li>${clothes[j].price} kr</li>
                             <div id="quantitys">
                             <button type="button" class="sub" id="${clothes[j].name}" title="If u want less quantity">-</button>
-                            <input type="text" value="${valdaItemsArray[itemName]}"  class="totalQuantity">
+                            <input type="text" value="${$valdaItemsArray[itemName]}"  class="totalQuantity">
                             <button type="button" class="add" id="${clothes[j].name}"title="If u want more quantity" >+</button>
                             </div>
                             </ul>`;
@@ -121,30 +123,30 @@ function showItems () {
 
 //Function för att kunna lägga till och ta bort items i varukorgen
 function addRemoveItems() {
-    let subBtn = document.getElementsByClassName('sub');     //ref till knapp för att ta bort varan
-    let addBtn = document.getElementsByClassName('add');    //ref till knapp för att lägga till vara på checkout sidan
+    $subBtn = $('.sub');     //ref till knapp för att ta bort varan
+    $addBtn = $('.add');    //ref till knapp för att lägga till vara på checkout sidan
 
-    for (var i = 0; i < subBtn.length; i++) {             //går genom knappar för att ta bort
-        subBtn[i].onclick = function () {                //lägger till eventlistener
+    for (var i = 0; i < $subBtn.length; i++) {             //går genom knappar för att ta bort
+        $subBtn[i].onclick = function () {                //lägger till eventlistener
             cartItemsCount--;
             let itemNames = this.id;                    //kollar om namnet är samma med denna id
             document.getElementById('visaAntalet').innerHTML = cartItemsCount;//skriver ut räknare vid varukorgen}
-            if (valdaItemsArray[itemNames] === 1) {
+            if ($valdaItemsArray[itemNames] === 1) {
                 let lastItem=itemsOf.indexOf(itemNames);
-                delete valdaItemsArray[itemNames];
+                delete $valdaItemsArray[itemNames];
                 itemsOf.splice(lastItem,1);
             }
             else {
-                valdaItemsArray[itemNames]--;
+                $valdaItemsArray[itemNames]--;
             }
             showItems();
         }
     }
 
-    for (var i = 0; i < addBtn.length; i++) {
-        addBtn[i].onclick = function () {
+    for (var i = 0; i < $addBtn.length; i++) {
+        $addBtn[i].onclick = function () {
             let itemNames = this.id;
-            valdaItemsArray[itemNames]++;
+            $valdaItemsArray[itemNames]++;
             cartItemsCount++;
             document.getElementById('visaAntalet').innerHTML = cartItemsCount;//skriver ut räknare vid varukorgen}
             showItems();
@@ -199,59 +201,69 @@ function validateForm() {
 document.getElementById('subBtn').addEventListener("click", validateForm);
 
 
-
-//lägger till click event på classe .linken (span elementer på product page ligger i den)
-
+//lägger till click event på class .linken (span elementer på product page ligger i den)
 $('.linken').on('click', function() {
-    $("#products").hide();//döljer producterna på första sidan
-    $itemNameforReview = this.id;
-    openReviewPage();
+    $("#products").hide();                // döljer produktena
+    $itemNameforReview = this.id;        // item som klickad på ska få this.id som är då klädplaggens namn
+    $('#myID').val($itemNameforReview); //myID får namnet på produkten som sin value
+
+    $('#stars span').on('click', function() {
+        $onStar = parseInt($(this).data('value'), 10); // The star currently selected
+        $('#stars').val($onStar);
+    });
+
+    openReviewPage();                  // öppnar reviewpage för att visa produkten som man klickat på
 });
 
 function openReviewPage() {
-    $("#reviewPage").show();
-    for (var j = 0; j < clothes.length; j++) {    //går genom alla kläd produkter o
-
-        if (clothes[j].name === $itemNameforReview) {
-            $('#itemForRew').html(
+    $("#reviewPage").show();                               //visar sidan då den är hide från början
+    for (var j = 0; j < clothes.length; j++) {            //går genom alla clothes
+        if (clothes[j].name === $itemNameforReview) {   //kollar om namn på clothes är samma som namnet på item som klickades på
+            $('#itemForRew').html(                      //skriver ut bilden, pris och url på denna item
                 `<img src="${clothes[j].url}">                                                                                                                   
-                     <li>${clothes[j].name}</li>                                                                                                                                               
-                     <lI>${clothes[j].price} kr </lI>                                                                                                                                   
+                     <h4>Snygg ${clothes[j].name} som är en av våran favoriter! Denna plagg kostar bara  ${clothes[j].price} kr !</h4>                                                                                                                                                                                                                                                                     
                   `)}
-        showReviews();
+        saveReviews();                              //anropar saveReviews functionen
+        printJSONDB("utskrift");                    //anropar funktionen för att skriva ut sparade reviews
+    }
+}
+function saveReviews() {
+    $reviewButtons = $('.reviewButtons');
+
+    for (var i = 0; i < $reviewButtons.length; i++) {
+        $reviewButtons[i].onclick = function () {
+            $myInputIDs = ["firstName","textareans","myID","stars"];        //sparar id på elements som jag vill skriva ut
+            makeJSONString($myInputIDs);                                   //anropar funktionen makeJSONSTRING och lägger in myInputIDs som parameter
+            printJSONDB("utskrift");
+            $('#textareans').val('');                                    //tömmer values efter dom har blivit skickade
+            $('#firstName').val('');
+        }
     }
 }
 
-function showReviews() {
-    $('#skickaRew').on('click', function () {
-
-        $nameInput = $('#firstName').val();
-        $messageInput = $('#textareans').val();
-        $('#nameOfuser').html(`Name: ${$nameInput}`);
-        $('#messageText').html(`Recension: ${$messageInput}`);
-
-        if(this.className === $itemNameforReview){
-        }
-        else {
-            //ändrar class namn efter review
-        }
-    });
+function makeJSONString (inputIDArray){
+    $tempInputArray = [];
+    for(var i = 0;i<inputIDArray.length;i++){                     //går genom arrayn med alla id's
+        $inputID = inputIDArray[i];
+        $temp = document.getElementById(inputIDArray[i]);
+        $input = '"' + $inputID + '":"' + $temp.value + '"';
+        $tempInputArray.push($input);
+    }
+    $arrayViews.push('{' + $tempInputArray.toString() +'}');
+    $inputArrayToString = $arrayViews.toString();
+    $jsonString = '{ "database" : [' + $inputArrayToString + ']}';
+    console.log($jsonString);
 }
 
-
-
-
-
-$('#textareans').val('');
-$('#firstName').val('');
-
-
-
-
-
-
-
-
-
+function printJSONDB (elementID) {
+    $dataToPrint = JSON.parse($jsonString);                         //gör  jasonString till object för att sen kunna jobba på den
+    $resultsField = document.getElementById(elementID);           //där ska resultatet skrivas ut
+    $resultsField.innerHTML = "";                                //tömmer
+    for (var i = 0; i < $arrayViews.length; i++) {
+        if ($dataToPrint.database[i].myID === $itemNameforReview) {
+            $resultsField.innerHTML += (" Stars: " + $dataToPrint.database[i].stars + "<br>" + " Name: " + $dataToPrint.database[i].firstName +  "<br>" + " Message: " + $dataToPrint.database[i].textareans + "<br>" + "<hr>");
+        }
+    }
+}
 
 
